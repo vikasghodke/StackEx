057a93d2139fd5a31a24ea36ac75cdc0a6dbb8d5
Vikas ghodke <vikasghodke468@gmail.com>
2024-02-02 15:44:55 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack;
+public class Program
+{
+
+
+    static void Main(string[] args)
+    {
+        Stack s1 = new Stack(7);
+        s1.push(2);
+        s1.push(4);
+        s1.push(5);
+        s1.push(8);
+        s1.push(9);
+
+        s1.pop();
+        s1.pop();
+        s1.show();
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack
+    {
+        public int[] arr;
+        public int top;
+        public int stacksize;
+
+        public Stack(int size)
+        {
+            top = -1;
+            arr = new int[size];
+            stacksize= size;
+        }
+        public void push(int value)
+        {
+            if(top!=stacksize-1)
+            {
+                arr[++top] = value;
+            }
+            else
+            {
+                Console.WriteLine("Stack is overflow");
+            }
+        }
+        public void pop()
+        {
+            if (top==-1)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                top--;
+            }
+        }
+        public int size()
+        {
+            return stacksize;
+        }
+        public void show()
+        {
+            for(int i = 0; i < top + 1; i++)
+            {
+                Console.WriteLine(arr[i]);  
+            }
+        }
+    }
+}
